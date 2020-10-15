88f581761703b727199af45d3db81ef9d1d2f2a5
Nikolay Shishkin <spwawnick@gmail.com>
2020-10-15 15:57:35 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if (n<0){
+        n = +String(n).split('').slice(1).join('');
+        return Number(String(n).split('').reverse().join(''));
+    }
+  return Number(String(n).split('').reverse().join(''));
 }
